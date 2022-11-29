b8b6b3140036f7f8df01336f29680644a857278d
MonikaGVS <109850304+MonikaGVS@users.noreply.github.com>
2022-11-29 15:36:01 +0530

    Add files via upload
    This Java Project consists of Online Reservation System features like Login, Reservation of tickets with PNR and Cancellation Page. And also any one can reserve any number of tickets.

---
+++ RailwayReservation.java
@@ -0,0 +1,10 @@
+
+public class RailwayReservation {
+
+    public void Reservation() {
+    }
+
+    public void printTicket(Object object, int i, int j, int k, Object object2, Object object3, boolean b) {
+    }
+
+}
